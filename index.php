bd93ca28894df8816c53875f903e65263ac601b2
sabino-olivieri <sabino.olivieri.28@gmail.com>
2024-06-03 16:46:52 +0200

    feat: created the "food" type for products

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <?php 
     require_once __DIR__ . "/models/product.php";
     require_once __DIR__ . "/models/category.php";
+    require_once __DIR__ . "/models/type_product/food.php";
 
-    $pettorina = new Product("Pettorina", 10);
     $dog_category = new Category("cane");
     $cat_category = new Category("gatto");
 
+    $pettorina = new Food("pettorina", 10.5, 500, "2024/10/5");
+    
     $pettorina->setDescription("pettorina molto bella");
     $pettorina->setImagePath("https://m.media-amazon.com/images/I/71JXn5GAfWL._AC_UF894,1000_QL80_.jpg");
     $pettorina->setCategory($dog_category);
---
+++ models/type_product/food.php
@@ -0,0 +1,61 @@
+<?php 
+class Food extends Product {
+    private int $weight;
+    private string $expirationDate;
+
+    public function __construct(string $_name, float $_price, int $_weight, string $_expirationDate)
+    {
+        parent::__construct($_name, $_price);
+
+        $this->setWeight($_weight);
+        $this->setExpirationDate($_expirationDate);
+    }
+
+    /**
+     * Get the value of weight
+     */ 
+    public function getWeight()
+    {
+        return $this->weight;
+    }
+
+    /**
+     * Set the value of weight
+     */ 
+    public function setWeight(int $_weight)
+    {
+        if($_weight > 0) {
+            $this->weight = $_weight;
+        } else {
+            throw new Exception("Il peso deve essere superiore a 0 grammi", 1);
+            
+        }
+
+    }
+
+    /**
+     * Get the value of expirationDate
+     */ 
+    public function getExpirationDate()
+    {
+        return $this->expirationDate;
+    }
+
+    /**
+     * Set the value of expirationDate
+
+     */ 
+    public function setExpirationDate(string $_expirationDate)
+    {   if(strtotime(trim($_expirationDate)) !== false) {
+
+        $date = date("d/m/Y", strtotime(trim($_expirationDate)));
+
+        $this->expirationDate = $date;
+    } else {
+        throw new Exception("Formato data errato! inserire il seguente formato AAAA/MM/DD", 1);
+        
+    }
+
+    }
+}
+?>
